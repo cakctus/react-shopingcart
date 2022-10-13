ae56e914bfafabc2cb742a57f5bedbe490de7e58
Cakctus <cakctusinc@gmail.com>
2022-10-13 20:05:23 +0300

    create components

--- lesson_1/src/App.tsx
+++ lesson_1/src/App.tsx
@@ -1,8 +1,33 @@
 import React from "react"
-import "./App.css"
+
+const List = () => {
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  )
+}
+
+const Title = () => {
+  return (
+    <div>
+      <h1>Title Component</h1>
+    </div>
+  )
+}
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <List />
+        <Title />
+        <h1>App Component</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
